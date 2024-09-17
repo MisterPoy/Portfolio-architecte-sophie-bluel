bdd6c198a4f37077e670f571f95fbe3e40a21edc
MrPoy <gregory.poupaux@hotmail.fr>
2024-09-17 14:23:56 +0200

    update
    debug login

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,13 +1,32 @@
 const formLogin = document.getElementById("loginForm-container");
 let baliseEmail = document.getElementById("loginEmail");
 let balisePassword = document.getElementById("password");
-let postLoginUrl = "http://localhost:5678/api/users/login";
+let postLoginUrl = 'http://localhost:5678/api/users/login';
 
-formLogin.addEventListener("submit", function(event){
+formLogin.addEventListener("submit",async function (event) {
     event.preventDefault();
-    let email = baliseEmail.value ;
+    let email = baliseEmail.value;
     let password = balisePassword.value;
-    console.log(email+"  "+password);
-    let reponseLogin = JSON.stringify({email, password});
-    console.log(reponseLogin);
-} );
+    console.log(email + "  " + password);
+    let loginPayload = JSON.stringify({ email, password });
+    console.log(loginPayload);
+
+    let userArray = [];
+        const loginUser = await fetch(postLoginUrl, {
+        method : 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body : loginPayload     
+});
+    userArray = await loginUser.json();
+    console.log(userArray);
+    let userToken = userArray.token;
+    console.log(userToken);
+    localStorage.setItem("token",userToken);
+});
+
+
+let token = localStorage.getItem("token");
+console.log(localStorage.token);
--- FrontEnd/assets/mainscript.js
+++ FrontEnd/assets/mainscript.js
@@ -1,3 +1,4 @@
+export {};
 
 // declaration globale worksArray en tableau vide afin qu'il soit remplit par la reponse.json()
 let worksArray = [];
@@ -7,12 +8,11 @@ worksArray = await reponseWorks.json();
 
 let gallery = document.querySelector(".gallery");
 
-function displayWorks(works){
+function displayWorks(works) {
   // erase html gallery
   gallery.innerHTML = "";
 
   for (let i = 0; i < works.length; i++) {
-
     const work = works[i];
 
     const worksFiles = document.createElement("figure");
@@ -28,22 +28,20 @@ function displayWorks(works){
 }
 displayWorks(worksArray);
 
-
 //declaration d'un tableau vide pour acceuillir les catégories
 let categoriesArray = [];
 
-// on récupère les données de l'api pour les catégorie, convertis en Tableau dans la constante categoriesArray 
+// on récupère les données de l'api pour les catégorie, convertis en Tableau dans la constante categoriesArray
 const reponse = await fetch("http://localhost:5678/api/categories");
 categoriesArray = await reponse.json();
 
 // on rajoute une catégorie au début du tableau pour TOUS sur le même modèle, avec une id de 0 et un name "tous"
-categoriesArray.unshift({"id": 0, "name": "Tous" });
-
+categoriesArray.unshift({ id: 0, name: "Tous" });
 
 let worksFilters_container = document.querySelector(".worksFilters_container");
 
 function createFilterBtn(categories) {
-//on vide le html du container des filtres
+  //on vide le html du container des filtres
   worksFilters_container.innerHTML = "";
   //boucle pour créer un élément html <button> pour chaque élément de categoriesArray, enfant du container principal et ayant un text qui correspond
   //au name de chaque catégorie
@@ -55,8 +53,8 @@ function createFilterBtn(categories) {
     categoriesBtn.classList.add("filterBtn");
     categoriesBtn.dataset.id = categorie.id;
 
-    categoriesBtn.addEventListener("click", function(){
-      let allFiltersBtn = document.querySelectorAll([".filterBtn"]);
+    categoriesBtn.addEventListener("click", function () {
+      let allFiltersBtn = document.querySelectorAll(".filterBtn");
       for (let i = 0; i < allFiltersBtn.length; i++) {
         allFiltersBtn[i].classList.remove("filterBtnActived");
       }
@@ -64,23 +62,19 @@ function createFilterBtn(categories) {
 
       const categoryId = parseInt(categoriesBtn.dataset.id);
       filterWorksByCategories(categoryId);
-
-    } );
+    });
   }
 }
 createFilterBtn(categoriesArray);
 
-
 function filterWorksByCategories(categoryId) {
-  
-  if (categoryId === 0){
+  if (categoryId === 0) {
     displayWorks(worksArray);
   } else {
-    const filteredWorks = worksArray.filter((work)=> work.categoryId === categoryId);
-    
+    const filteredWorks = worksArray.filter(
+      (work) => work.categoryId === categoryId
+    );
+
     displayWorks(filteredWorks);
   }
 }
-
-
-

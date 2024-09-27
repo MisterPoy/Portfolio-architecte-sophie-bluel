b12185f37c56708ba7ede98321b643bb1564a9fc
MrPoy <gregory.poupaux@hotmail.fr>
2024-09-27 13:42:30 +0200

    update

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,17 +1,27 @@
+
+
+
+
+// je récupère le formulaire html dans une constante formLogin pour pouvoir "écouter" lévènement submit
 const formLogin = document.getElementById("loginForm-container");
+// récupération des champs de saisies email et pass word
 let baliseEmail = document.getElementById("loginEmail");
 let balisePassword = document.getElementById("password");
+//stockage de l'url de la demande de login API
 let postLoginUrl = 'http://localhost:5678/api/users/login';
 
-formLogin.addEventListener("submit",async function (event) {
+    // écouteur d'évènements sur ll'évènement "submit" du formulaire
+formLogin.addEventListener("submit", async function (event) {
+    // on coupe le comportement par défaut du nav pour qu'il ne recharge pas la page
     event.preventDefault();
+    // on récupère les valeures saisi par l'utilisateur dans les champs de saisie pour la stocker
     let email = baliseEmail.value;
     let password = balisePassword.value;
-    console.log(email + "  " + password);
+    // on converti l'email et password en JSON pour la requète API en variable loginPayload
     let loginPayload = JSON.stringify({ email, password });
     console.log(loginPayload);
 
-    let userArray = [];
+    
     try {
         const loginUser = await fetch(postLoginUrl, {
             method : 'POST',
@@ -21,33 +31,34 @@ formLogin.addEventListener("submit",async function (event) {
             },
             body : loginPayload     
         });
+        console.log(loginUser);
         if (loginUser.ok) {
-            userArray = await loginUser.json();
+            let userArray = await loginUser.json();
             console.log(userArray);
             let userToken = userArray.token;
             console.log(userToken);
             localStorage.setItem("token",userToken);
             window.location.href = 'index_edition.html';
-        } else { 
-            window.alert("Erreur dans l'identifiant ou le mot de passe"); 
+            /* closeModaleWithXmark(); */
+        } else if (loginUser.status === 404) {
+            // Si le statut est 401, affiche un message personnalisé et empêche l'erreur par défaut
+            console.log('Erreur dans l’identifiant ou le mot de passe (401)');
+            window.alert('Erreur dans l’identifiant ou le mot de passe');
+        } else if (loginUser.status === 401) {
+            // Pour d'autres codes d'erreur
+            console.log(`Erreur : ${loginUser.status}`);
+            window.alert('Erreur dans l’identifiant ou le mot de passe');
         }
     } catch (error) {
-        console.log(error);
-        window.alert(error);   
+        // Gestion des erreurs réseaux ou autres exceptions
+        console.error(error);
+        window.alert('Une erreur réseau est survenue. Veuillez vérifier votre connexion.');
     }
 });
 
 
 let token = localStorage.getItem("token");
 console.log(localStorage.token);
-
-
-    
-        
-    
-
-    
-    
 
     
 
--- FrontEnd/assets/mainscript.js
+++ FrontEnd/assets/mainscript.js
@@ -5,6 +5,7 @@ let worksArray = [];
 
 const reponseWorks = await fetch("http://localhost:5678/api/works");
 worksArray = await reponseWorks.json();
+console.log(worksArray);
 
 let gallery = document.querySelector(".gallery");
 
@@ -29,6 +30,7 @@ function displayWorks(works) {
 displayWorks(worksArray);
 
 
+
 //declaration d'un tableau vide pour acceuillir les catégories
 let categoriesArray = [];
 
@@ -39,19 +41,18 @@ categoriesArray = await reponse.json();
 // on rajoute une catégorie au début du tableau pour TOUS sur le même modèle, avec une id de 0 et un name "tous"
 categoriesArray.unshift({ id: 0, name: "Tous" });
 
-let worksFilters_container = document.querySelector(".worksFilters_container");
+let worksFiltersContainer = document.querySelector(".worksFilters_container");
 
-
-
+//FONCTION DE CREATION DES FILTRES
 function createFilterBtn(categories) {
   //on vide le html du container des filtres
-  worksFilters_container.innerHTML = "";
+  worksFiltersContainer.innerHTML = "";
   //boucle pour créer un élément html <button> pour chaque élément de categoriesArray, enfant du container principal et ayant un text qui correspond
   //au name de chaque catégorie
   for (let i = 0; i < categories.length; i++) {
     let categorie = categories[i];
     let categoriesBtn = document.createElement("button");
-    worksFilters_container.appendChild(categoriesBtn);
+    worksFiltersContainer.appendChild(categoriesBtn);
     categoriesBtn.innerText = categorie.name;
     categoriesBtn.classList.add("filterBtn");
     categoriesBtn.dataset.id = categorie.id;
@@ -68,7 +69,12 @@ function createFilterBtn(categories) {
     });
   }
 }
-createFilterBtn(categoriesArray);
+/* createFilterBtn(categoriesArray);  */
+
+
+const token = localStorage.getItem('token');
+console.log(token)
+
 
 function filterWorksByCategories(categoryId) {
   if (categoryId === 0) {
@@ -77,7 +83,43 @@ function filterWorksByCategories(categoryId) {
     const filteredWorks = worksArray.filter(
       (work) => work.categoryId === categoryId
     );
-
     displayWorks(filteredWorks);
-  }
+    console.log(categoryId);
+    }
 }
+
+
+
+//TEST DE CODE POUR FERMER LA MODALEAVEC LA CROIX
+function closeModaleWithXmark (){
+  let xmark = document.querySelector('.fa-xmark');
+  console.log(xmark);
+  let modale = document.getElementById('modaleContainer');
+  console.log(modale);
+
+  xmark.addEventListener("click", function() {
+    modaleWindows.id = 'modaleContainer'
+  })
+}
+
+/* closeModaleWithXmark(); */
+
+const launchModalButton = document.querySelector('.modify_button');
+console.log(launchModalButton);
+const modaleWindows = document.querySelector('.modale_main-container');
+console.log(modaleWindows);
+const modale = document.querySelector('.modale');
+console.log(modale);
+launchModalButton.addEventListener('click', function(){
+  modaleWindows.id = '';
+  console.log("Hello there !")
+});
+
+/* curl -X 'POST' \
+  'http://localhost:5678/api/users/login' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "email": "string",
+  "password": "string"
+}' */
